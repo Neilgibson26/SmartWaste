1492d51b0c2e175b80b1dd3631ad95903898fa69
Neil Gibson <1neilgibson1@gmail.com>
2023-11-19 22:04:06 +0000

    Initial Commit

---
+++ SmartWaste/SmartWasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartWasteApp.swift
+//  SmartWaste
+//
+//  Created by Neil Gibson on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartWasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
